6eed8d7a41bdab2dd9f03d253ecda952a64796de
JeyaVardhan-L <68065109+JeyaVardhan-L@users.noreply.github.com>
2026-09-06 19:09:15 +0530

    Add files API

--- server/app.py
+++ server/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, send_from_directory
+from pathlib import Path
 
 app = Flask(__name__)
+
+BATCAVE_FILES = Path.home() / "storage" / "shared" / "BatCave" / "files"
 
 @app.route("/")
 def home():
@@ -9,6 +12,12 @@ def home():
 @app.route("/<path:path>")
 def static_files(path):
     return send_from_directory("../web", path)
+
+@app.route("/api/files")
+def files():
+    return {
+        "files": [file.name for file in BATCAVE_FILES.iterdir() if file.is_file()]
+    }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
