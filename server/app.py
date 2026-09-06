d078d23bf630f7394615479ccd79b6acf92028cc
JeyaVardhan-L <68065109+JeyaVardhan-L@users.noreply.github.com>
2026-09-06 19:07:06 +0530

    Serve web interface through Flask

--- server/app.py
+++ server/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>🦇 Batcave is alive.</h1>"
+    return send_from_directory("../web", "index.html")
+
+@app.route("/<path:path>")
+def static_files(path):
+    return send_from_directory("../web", path)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
